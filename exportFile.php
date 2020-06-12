41837574feb4a86294ac53a4467969495a592154
denshade <lieven.vaneeckhaute@uzgent.be>
2020-06-12 16:08:46 +0200

    feedback processed.

--- exportFile.php
+++ exportFile.php
@@ -7,31 +7,33 @@ if ($pid === null) {
     die("Invalid request, pid missing");
 }
 
+
+$Proj = new Project($pid);
+$datestamp = new DateTime();
 global $module;
 header("Content-type: text/csv");
-header("Content-Disposition: attachment; filename=rights.csv");
+header("Content-Disposition: attachment; filename=".$Proj->project["project_name"]."-userpermissions-".$datestamp->format("Y-m-d").".csv");
 header("Pragma: no-cache");
 header("Expires: 0");
 /**
  * @var $module \uzgent\ExportUserRoleDag\ExportUserRoleDag
  */
 if ($storeheader == "on") {
-    echo "User name,role name,data access group\n";
+    echo "User name,Role name,Data access group,Expiration date\n";
 }
 $userrights = UserRights::getRightsAllUsers();
 $roles =  UserRights::getRoles();
-$Proj = new Project($pid);
 $dags = $Proj->getGroups();
 if ($dags == null) {
     foreach($userrights as $username => $userdetails)
     {
-        echo $username.",".$roles[$userdetails["role_id"]]['role_name']."\n";
+        echo $username.",".$roles[$userdetails["role_id"]]['role_name'].",".$userdetails['expiration']."\n";
     }
 }
 else {
     foreach($userrights as $username => $userdetails)
     {
-        echo $username.",".$roles[$userdetails["role_id"]]['role_name'].",".$dags[$userdetails["group_id"]]."\n";
+        echo $username.",".$roles[$userdetails["role_id"]]['role_name'].",".$dags[$userdetails["group_id"]].",".$userdetails['expiration']."\n";
     }
 }
 
